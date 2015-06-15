c36cb4fbbff16779b9749d567b6f2d5d3fbd8823
Wei-Lun Hung <kinmener@gmail.com>
2015-06-15 08:25:36 -0700

    fix numThread bug in q1 of hw1

--- homework/hw1/template/q1/Main.java
+++ homework/hw1/template/q1/Main.java
@@ -16,6 +16,9 @@ public class Main {
             System.exit(-1);
         }
 
+        numThread = Integer.parseInt(args[1]);
+        numTotalInc = Integer.parseInt(args[2]);
+
         if (args[0].equals("fast")) {
             lock = new FastMutexLock(numThread);
             counter = new LockCounter(lock);
@@ -31,8 +34,6 @@ public class Main {
             System.exit(-1);
         }
 
-        numThread = Integer.parseInt(args[1]);
-        numTotalInc = Integer.parseInt(args[2]);
 
         // TODO
         // Please create numThread threads to increment the counter
