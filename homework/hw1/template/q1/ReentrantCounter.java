409f28072f302357b6dc54f941442afaa398d025
YoinkBird <email>
2015-06-18 02:11:12 -0500

    hw1q2: implemented reentrant

--- homework/hw1/template/q1/ReentrantCounter.java
+++ homework/hw1/template/q1/ReentrantCounter.java
@@ -1,7 +1,17 @@
+import java.util.concurrent.locks.ReentrantLock;
+
 // TODO
 // Use ReentrantLock to protect the count
+// http://docs.oracle.com/javase/7/docs/api/java/util/concurrent/locks/ReentrantLock.html
 public class ReentrantCounter extends Counter {
+  private final ReentrantLock lock = new ReentrantLock();
     @Override
     public void increment() {
+        lock.lock(); // block until condition holds
+        try{
+            this.count++;
+        } finally {
+            lock.unlock();
+        }
     }
 }
